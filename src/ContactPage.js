009c770f34effdc0f9d33b68cc62e395978a32ce
JILLIAN HONRADE <jhonrade@uw.edu>
2016-11-21 00:22:27 -0800

    basic setupp

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -1,5 +1,6 @@
 // Contact Page
 import React from 'react';
+import './css/ContactPage.css';
 
 // PageOne Component
 var ContactPage = React.createClass({
@@ -9,7 +10,29 @@ var ContactPage = React.createClass({
 			<div className="contactPage">
 				<div>
 					<h2>Contact</h2>
+
 				</div>
+
+				<form action="mailto:jhonrade@uw.edu" method="post">
+					<div>
+		        <label for="name">Name:</label>
+		        <input type="text" id="name" name="user_name" />
+					</div>
+					<div>
+		        <label for="mail">E-mail:</label>
+		        <input type="email" id="mail" name="user_email" />
+					</div>
+					<div>
+		        <label for="msg">Message:</label>
+		        <textarea id="msg" name="user_message"></textarea>
+					</div>
+
+					<div class="button">
+		        <button type="submit">Send your message</button>
+					</div>
+					
+				</form>
+
 			</div>
 		);
 	}
--- src/Project.js
+++ src/Project.js
@@ -1,24 +1,28 @@
 import React from 'react';
-//import { Button, Card, Row, Col } from 'react-materialize';
-import Card from '../node_modules/react-materialize/lib/Card.js';
-import Col from '../node_modules/react-materialize/lib/Col.js';
+import { Button, Card, Row, Col } from 'react-materialize';
+// import Card from '../node_modules/react-materialize/lib/Card.js';
+// import Col from '../node_modules/react-materialize/lib/Col.js';
+
+//import { Button, Card, Row, Col } from 'material-ui';
+//import Card from '../node_modules/material-ui/Card/Card.js';
 
 var Project = React.createClass({
     render() {
         return(
-          // <div>
-          //   <Col m={6} s={12}>
-          //     <Card className='projectCard' textClassName='projectCardText' title={this.props.title} actions={[<a href={this.props.link}>Check Out More!</a>]}>
-          //       {this.props.description}
-          //     </Card>
-          //   </Col>
-          // </div>
-
-          <div className="projects">
-            <h3>{this.props.title}</h3>
-            <p>{this.props.description}</p>
-            <a target="_blank" href={this.props.link}> Check Out More!</a>
+          <div>
+            <Col m={6} s={12}>
+              <Card className='projectCard' textClassName='projectCardText' title={this.props.title} actions={[<a href={this.props.link}>Check Out More!</a>]}>
+                {this.props.description}
+              </Card>
+            </Col>
           </div>
+
+
+          // <div className="projects">
+          //   <h3>{this.props.title}</h3>
+          //   <p>{this.props.description}</p>
+          //   <a target="_blank" href={this.props.link}> Check Out More!</a>
+          // </div>
 
         )
     }
